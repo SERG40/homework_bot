01d0f0585cc44e6252bac2c8ca409453ae1ef73c
Сергей Кастов <smailix1@gmail.com>
2022-05-02 09:29:46 +0300

    Работает, пора проверять.

--- homework.py
+++ homework.py
@@ -1,9 +1,18 @@
 import os
+import logging
+import sys
+import json
 import requests
 import time
 
 from telegram import Bot
 from dotenv import load_dotenv
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s, %(levelname)s, %(message)s',
+    handlers=[logging.FileHandler('log.txt'),
+              logging.StreamHandler(sys.stdout)])
 
 load_dotenv()
 
@@ -25,62 +34,89 @@ HOMEWORK_STATUSES = {
 
 
 def send_message(bot, message):
+    """Отправляем сообщение в ТГ."""
     return bot.send_message(TELEGRAM_CHAT_ID, message)
 
 
 def get_api_answer(current_timestamp):
-    timestamp = current_timestamp or int(time.time())
-    params = {'from_date': timestamp}
-    homework_statuses = requests.get(ENDPOINT, headers=HEADERS, params=params)
-    return homework_statuses.json()
+    """Получаем API."""
+    params = {'from_date': current_timestamp}
+    try:
+        homework_statuses = requests.get(ENDPOINT, headers=HEADERS,
+                                         params=params)
+    except requests.exceptions.RequestException as error:
+        logging.error(f'Сервер Яндекс.Практикум вернул ошибку: {error}.')
+        send_message(f'Сервер Яндекс.Практикум вернул ошибку: {error}.')
+    try:
+        return homework_statuses.json()
+    except json.JSONDecodeError:
+        logging.error('Сервер вернул невалидный json.')
+        send_message('Сервер вернул невалидный json.')
 
 
 def check_response(response):
-    pass
+    """Проверяем response."""
+    homeworks = response['homeworks']
+    if homeworks is None:
+        raise KeyError('Не содержит ключ или пустое значение.')
+    if not isinstance(homeworks, list):
+        raise TypeError('Неверный формат homework.')
+    if not homeworks:
+        return False
+    homeworks_status = response['homeworks'][0].get('status')
+    if homeworks_status in HOMEWORK_STATUSES:
+        return homeworks
+    else:
+        raise KeyError('Отсутствие или неверный статус работы.')
 
 
-# def parse_status(homework):
-#     homework_name = ...
-#     homework_status = ...
+def parse_status(homework):
+    """Проверяем статус."""
+    try:
+        homework_name = homework['homework_name']
+    except KeyError:
+        logging.error('Неверный ответ сервера')
 
-#     ...
+    homework_status = homework.get('status')
+    verdict = ''
+    if ((homework_status is None) or (
+        homework_status == '')) or ((
+            homework_status != 'approved') and (
+            homework_status != 'rejected')):
+        logging.error(f'Статус работы некорректен: {homework_status}')
+    if homework_status == 'rejected':
+        verdict = 'Работа проверена: у ревьюера есть замечания.'
+    elif homework_status == 'approved':
+        verdict = 'Работа проверена: ревьюеру всё понравилось. Ура!'
+    elif homework_status == 'reviewing':
+        verdict = 'Работа взята на проверку ревьюером.'
+    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
-#     verdict = ...
 
-#     ...
-
-#     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
-
-
-# def check_tokens():
-#     ...
+def check_tokens():
+    """Проверка токенов."""
+    if all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]):
+        logging.critical('Отсутствие ID или Токена')
+        return True
+    return False
 
 
-# def main():
-#     """Основная логика работы бота."""
+def main():
+    """Основная логика работы бота."""
+    current_timestamp = int(time.time())
+    while True:
+        try:
+            all_homework = get_api_answer(current_timestamp)
+            if len(all_homework['homeworks']) > 0:
+                homework = all_homework['homeworks'][0]
+                send_message(bot, parse_status(homework))
+                logging.info('Сообщение отправлено')
+            time.sleep(RETRY_TIME)
 
-#     ...
-
-#     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-#     current_timestamp = int(time.time())
-
-#     ...
-
-#     while True:
-#         try:
-#             response = ...
-
-#             ...
-
-#             current_timestamp = ...
-#             time.sleep(RETRY_TIME)
-
-#         except Exception as error:
-#             message = f'Сбой в работе программы: {error}'
-#             ...
-#             time.sleep(RETRY_TIME)
-#         else:
-#             ...
+        except Exception as error:
+            logging.error(f'Сбой в работе программы: {error}')
+            send_message(f'Сбой в работе программы: {error}')
+            time.sleep(RETRY_TIME)
 
 
 if __name__ == '__main__':
