aed7a9f91ecbf3aba7438b1cd61ecf19e85e455e
Сергей Кастов <smailix1@gmail.com>
2022-05-04 14:06:23 +0300

    Доработка check_response и parse_status

--- homework.py
+++ homework.py
@@ -5,6 +5,7 @@ import json
 import requests
 import time
 
+from http import HTTPStatus
 from telegram import Bot
 from dotenv import load_dotenv
 
@@ -15,6 +16,7 @@ logging.basicConfig(
               logging.StreamHandler(sys.stdout)])
 
 load_dotenv()
+logger = logging.getLogger('logger')
 
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
@@ -24,7 +26,6 @@ TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-bot = Bot(token=TELEGRAM_TOKEN)
 
 HOMEWORK_STATUSES = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
@@ -35,7 +36,16 @@ HOMEWORK_STATUSES = {
 
 def send_message(bot, message):
     """Отправляем сообщение в ТГ."""
-    return bot.send_message(TELEGRAM_CHAT_ID, message)
+    try:
+        bot.send_message(TELEGRAM_CHAT_ID, message)
+        logger.info(f'Бот отправил сообщение {message}')
+    except Exception:
+        logger.error(
+            f'Сбой при отправке в Телеграмм сообщения "{message}"',
+            exc_info=True
+        )
+        return False
+    return True
 
 
 def get_api_answer(current_timestamp):
@@ -44,75 +54,86 @@ def get_api_answer(current_timestamp):
     try:
         homework_statuses = requests.get(ENDPOINT, headers=HEADERS,
                                          params=params)
+        if homework_statuses.status_code != HTTPStatus.OK:
+            logger.error(f'Ошибка API {homework_statuses.status_code}')
+            raise ConnectionError('Ошибка соединения')
     except requests.exceptions.RequestException as error:
-        logging.error(f'Сервер Яндекс.Практикум вернул ошибку: {error}.')
+        logger.error(f'Сервер Яндекс.Практикум вернул ошибку: {error}.')
         send_message(f'Сервер Яндекс.Практикум вернул ошибку: {error}.')
     try:
         return homework_statuses.json()
     except json.JSONDecodeError:
-        logging.error('Сервер вернул невалидный json.')
+        logger.error('Сервер вернул невалидный json.')
         send_message('Сервер вернул невалидный json.')
 
 
 def check_response(response):
     """Проверяем response."""
-    homeworks = response['homeworks']
-    if homeworks is None:
-        raise KeyError('Не содержит ключ или пустое значение.')
-    if not homeworks:
-        return False
-    homeworks_status = response['homeworks'][0].get('status')
-    if homeworks_status in HOMEWORK_STATUSES:
-        return homeworks
-    else:
-        raise KeyError('Отсутствие или неверный статус работы.')
+    if type(response) is not dict:
+        raise TypeError('Ошибка типа')
+    try:
+        homeworks_list = response.get('homeworks')
+    except KeyError:
+        logger.error('Неверный ключ')
+        raise KeyError('Неверный ключ')
+    try:
+        homeworks_list[0]
+    except IndexError:
+        logger.error('Нет домашней работы')
+        raise IndexError('Нет домашней работы')
+    return homeworks_list
 
 
 def parse_status(homework):
     """Проверяем статус."""
-    try:
-        homework_name = homework['homework_name']
-    except KeyError:
-        logging.error('Неверный ответ сервера')
+    pprint(homework)
+    if isinstance(homework, dict):
+        homework_name = homework.get('homework_name')
+        homework_status = homework.get('status')
 
-    homework_status = homework.get('status')
-    verdict = ''
-    if ((homework_status is None) or (
-        homework_status == '')) or ((
-            homework_status != 'approved') and (
-            homework_status != 'rejected')):
-        logging.error(f'Статус работы некорректен: {homework_status}')
-    if homework_status == 'rejected':
-        verdict = 'Работа проверена: у ревьюера есть замечания.'
-    elif homework_status == 'approved':
-        verdict = 'Работа проверена: ревьюеру всё понравилось. Ура!'
-    elif homework_status == 'reviewing':
-        verdict = 'Работа взята на проверку ревьюером.'
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+        if homework_status not in HOMEWORK_STATUSES:
+            logger.error('Недокументированный статус')
+            raise KeyError('Отстутствует ключ')
+        else:
+            verdict = HOMEWORK_STATUSES.get(homework_status)
+            if homework_name is not None:
+                return (f'Изменился статус проверки работы "{homework_name}".'
+                        f'{verdict}')
+    else:
+        logger.error('Ошибка')
+        return f'Ошибка типа! Проверь {homework}'
 
 
 def check_tokens():
-    """Проверка токенов."""
+    """Проверяет доступность токенов."""
     if all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]):
-        logging.critical('Отсутствие ID или Токена')
         return True
+    else:
+        if PRACTICUM_TOKEN is None:
+            logger.critical('отсутствует : PRACTICUM_TOKEN')
+        if TELEGRAM_TOKEN is None:
+            logger.critical('отсутствует : TELEGRAM_TOKEN')
+        if TELEGRAM_CHAT_ID is None:
+            logger.critical('отсутствует : TELEGRAM_CHAT_ID')
     return False
 
 
 def main():
     """Основная логика работы бота."""
-    current_timestamp = int(time.time())
+    current_timestamp = 0
+    bot = Bot(token=TELEGRAM_TOKEN)
+    if check_tokens() is not True:
+        raise ValueError('Беда с токенами.')
     while True:
         try:
             all_homework = get_api_answer(current_timestamp)
             if len(all_homework['homeworks']) > 0:
                 homework = all_homework['homeworks'][0]
                 send_message(bot, parse_status(homework))
-                logging.info('Сообщение отправлено')
+                logger.info('Сообщение отправлено')
             time.sleep(RETRY_TIME)
-
         except Exception as error:
-            logging.error(f'Сбой в работе программы: {error}')
+            logger.error(f'Сбой в работе программы: {error}')
             send_message(f'Сбой в работе программы: {error}')
             time.sleep(RETRY_TIME)
 
